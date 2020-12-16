a520632643948a80db080566b2e42d2ebe04c204
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2020-12-16 19:30:27 +0800

    Update java_sol.java

--- java_sol.java
+++ java_sol.java
@@ -2,30 +2,57 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static int GCD(int n1, int n2) {
-		int gcd = 1;
-		for (int i = 1; i <= n1 && i <= n2; i++) {
-			if (n1 % i == 0 && n2 % i == 0) {
-				gcd = i;
-			}
-		}
-		return gcd;
+	static final Reader in = new Reader();
+	static final PrintWriter out = new PrintWriter(System.out);
+
+	public static void main(String[] args) {
+		int t=in.nextInt();
+		for(int i=1; i<=t; ++i)
+			new Solver();
+		out.close();
 	}
 	
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		int tt = in.nextInt();
-		for (int qq = 1; qq <= tt; qq++) {
-			int n = in.nextInt();
+	static class Solver {
+		Solver() {
+			int n=in.nextInt();
 			int[] a = new int[n];
-			for (int i = 0; i < n; i++) {
-				a[i] = in.nextInt();
-			}
-			int tmp = a[0];
-			for (int i = 1; i < n; i++) {
-				tmp = GCD(tmp, a[i]);
-			}
-			System.out.println(tmp == 1 ? 0 : -1);
+			for(int i=0; i<n; ++i)
+				a[i]=in.nextInt();
+			int tmp=a[0];
+			for(int i=1; i<n; ++i)
+				tmp=gcd(tmp, a[i]);
+			out.println(tmp==1?0:-1);
+		}
+		
+		static int gcd(int n1, int n2) {
+			int gcd=1;
+			for(int i=1; i<=n1&&i<=n2; ++i)
+				if(n1%i==0&&n2%i==0)
+					gcd=i;
+			return gcd;
 		}
 	}
+	
+	static class Reader {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		String next() {
+			while(st==null||!st.hasMoreTokens()) {
+				try {
+					st=new StringTokenizer(in.readLine());
+				} catch(Exception e) {}
+			}	
+			return st.nextToken();
+		}
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+	}
+	
 }
